4625c1c39bda37ff5eeee3722c080ef8a9555ce3
DELTA\john.tc.tsai <john.tc.tsai@deltaww.com>
2022-02-08 15:56:50 +0800

    Update to ver 0.1.0.

--- Runtime/Scripts/FFAudioRecorder.cs
+++ Runtime/Scripts/FFAudioRecorder.cs
@@ -1,192 +1,137 @@
-﻿// Sources: https://www.twblogs.net/a/5ef0c1884b16c91a2849601a
+﻿// 
+//  Created by John.Tsai on 2022/2/8
+//  Copyright © 2022 John.Tsai. All rights reserved.
+//
 
 using System;
-using System.IO;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class FFAudioRecorder : MonoBehaviour
 {
-    #region 模擬控制檯信號需要使用的DLL
-    [DllImport("kernel32.dll")]
-    static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
-    [DllImport("kernel32.dll")]
-    static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
-    [DllImport("kernel32.dll")]
-    static extern bool AttachConsole(int dwProcessId);
-    [DllImport("kernel32.dll")]
-    static extern bool FreeConsole();
+    #region 
+    [DllImport("user32.dll", EntryPoint = "SetWindowText", CharSet = CharSet.Ansi)]
+    public static extern int SetWindowText(int hwnd, string lpString);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
     #endregion
 
     [Tooltip("StreamingAssetsPath + [FFmpegPath]")]
-    public string FFmpegPath = "/ffmpeg/ffmpeg.exe";
+    public string ffmpegPath = "/ffmpeg/ffmpeg.exe"; 
     public string AudioInput = "Microphone (Realtek(R) Audio)";
+    public bool showfflog = true;
+
+    public string OutputFolder = "Output";
 
     [Tooltip("If 'CustomFileName' is empty, it'll export by default name.")]
     public string CustomFileName = string.Empty;
-    public bool showlog = true;
+
+    public string GetOutputName { get; set; }
 
     private string ffpath = string.Empty;
-    private int _pid;
-    private bool _isRecording = false;
+    //private int processId = 0;
+    private Process process = null;
+    private Process process_console = null;
 
-
-    private void Start()
+    void Start()
     {
-        ffpath = Application.streamingAssetsPath + FFmpegPath;
+        ffpath = @Application.streamingAssetsPath + ffmpegPath;
 
-        // Clear ffmpeg Process in memory.
-        StartCoroutine(IEExitCmd(() => { print("Init"); }, 0));
-        Process[] goDie = Process.GetProcessesByName("ffmpeg");
-        foreach (Process p in goDie) p.Kill();
+        if (!Directory.Exists(Path.Combine(Application.streamingAssetsPath, OutputFolder).TrimEnd('/')))
+            Directory.CreateDirectory(Path.Combine(Application.streamingAssetsPath, OutputFolder).TrimEnd('/'));
+
+        IntPtr handle = GetForegroundWindow();
+        SetWindowText(handle.ToInt32(), Application.productName);
     }
 
-    private void OnDestroy()
+    private void OnApplicationQuit()
     {
-        if (_isRecording)
+        if (process != null)
         {
-            try
-            {
-                UnityEngine.Debug.LogError("FFRecorder::OnDestroy - 錄製進程非正常結束，輸出文件可能無法播放。");
-                Process.GetProcessById(_pid).Kill();
-            }
-            catch (Exception e)
-            {
-                UnityEngine.Debug.LogError("FFRecorder::OnDestroy - " + e.Message);
-            }
+            process.StandardInput.WriteLine("q");
+            process.WaitForExit();
+            process.Close();
+        }
+
+        if (process_console != null)
+        {
+            //process_console.StandardInput.WriteLine("q");
+            process_console.WaitForExit();
+            process_console.Close();
         }
     }
-
 
     [ContextMenu("GetDevicesList")]
     public void GetDevicesList()
     {
-        print("[GetDevicesList]");
-        if (_isRecording)
-        {
-            UnityEngine.Debug.LogError("FFRecorder::StartRecording - 當前已有錄製進程。");
-            return;
-        }
-
-        var _ffargs = "-list_devices true -f dshow -i dummy";
-        StartCoroutine(GetDevicesProcess(_ffargs));
+        var args = "-list_devices true -f dshow -i dummy";
+        Process(ref process_console, args); 
     }
 
     [ContextMenu("StartRecording")]
     public void StartRecording()
     {
-        if (_isRecording)
-        {
-            UnityEngine.Debug.LogError("FFRecorder::StartRecording - 當前已有錄製進程。");
-            return;
-        }
-
-        // 解析設置，如果設置正確，則開始錄製
-        print("[Start FF]");
-        var _ffargs = SettingRecordingArgs();
-        UnityEngine.Debug.Log("FFRecorder::StartRecording - 執行命令：" + ffpath + " " + _ffargs);
-        StartCoroutine(IEEnterCmd(_ffargs));
-    }
-
-    [ContextMenu("StopRecording")]
-    public void StopRecording()
-    {
-        StopRecording(() =>
-        {
-            print("[Stop FF]");
-        });
-    }
-
-
-    private void StopRecording(Action _OnStopRecording)
-    {
-        if (!_isRecording)
-        {
-            UnityEngine.Debug.Log("FFRecorder::StopRecording - 當前沒有錄製進程，已取消操作。");
-            return;
-        }
-        StartCoroutine(IEExitCmd(_OnStopRecording));
-    }
-
-    private string SettingRecordingArgs()
-    {
-        var path = Path.Combine(Application.streamingAssetsPath, "output");
+        print("[Log] Start Recording...");
+        // Set output path
+        var path = Path.Combine(Application.streamingAssetsPath, OutputFolder);
         string fileName = CustomFileName.Equals(String.Empty) ?
                           Path.Combine(path, Application.productName + "_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".mp3") :
                           Path.Combine(path, CustomFileName + ".mp3");
-        var _ffargs = "-f dshow -i audio=\"" + AudioInput + "\" -acodec libmp3lame " + " \"" + fileName + "\"";
-        return _ffargs;
+        var args = "-f dshow -i audio=\"" + AudioInput + "\" -acodec libmp3lame " + " \"" + fileName + "\"";
+        GetOutputName = fileName;
+
+        var task = Task.Run(() =>
+        {
+            Process(ref process, args);
+        });
     }
 
+    [ContextMenu("StopRecording")]
+    async public void StopRecording()
+    {
+        if (process != null)
+        {
+            print("[Log] Stop Record");
+            process.StandardInput.WriteLine("q");
+            process.WaitForExit();
+            process.Close();
+            process = null;
+
+            await Task.Delay(1000);
+            print("[Log] Output: " + GetOutputName);
+        }
+    }
+
+    private void Process(ref Process tprocess, string args)
+    {
+        tprocess = new Process();
+        tprocess.StartInfo.FileName = ffpath;
+
+        // Set args into ffmepg
+        tprocess.StartInfo.Arguments = args;
+        tprocess.StartInfo.UseShellExecute = false;
+        tprocess.StartInfo.RedirectStandardError = true;
+        tprocess.StartInfo.RedirectStandardInput = true;
+        tprocess.StartInfo.RedirectStandardOutput = true;
+        tprocess.StartInfo.CreateNoWindow = true;
+
+        if (showfflog)
+            tprocess.ErrorDataReceived += new DataReceivedEventHandler(Output);
+
+        tprocess.Start();
+        //processId = tprocess.Id;
+        //UnityEngine.Debug.Log("processId:" + processId);
+        tprocess.BeginErrorReadLine();
+    }
     private void Output(object sendProcess, DataReceivedEventArgs output)
     {
         if (!string.IsNullOrEmpty(output.Data))
-        {
             UnityEngine.Debug.Log(output.Data);
-            if (output.Data.Contains("I/O error"))
-                UnityEngine.Debug.LogError(output.Data);
-        }
-    }
-
-    private IEnumerator GetDevicesProcess(string args)
-    {
-        yield return IEEnterCmd(args);
-        StartCoroutine(IEExitCmd(
-            () => { print("[Stop FF]"); }, 1));
-    }
-
-    private IEnumerator IEEnterCmd(string args)
-    {
-        yield return null;
-
-        Process ffp = new Process();
-        ffp.StartInfo.FileName = ffpath;                   // 進程可執行文件位置
-        ffp.StartInfo.Arguments = args;                  // 傳給可執行文件的命令行參數
-        ffp.StartInfo.CreateNoWindow = true;             // 是否顯示控制檯窗口
-        ffp.StartInfo.UseShellExecute = false;             // 是否使用操作系統Shell程序啓動進程
-
-        //ffp.StartInfo.CreateNoWindow = !debugOpenFF;             // 是否顯示控制檯窗口
-        //ffp.StartInfo.UseShellExecute = debugOpenFF;             // 是否使用操作系統Shell程序啓動進程
-        ffp.StartInfo.Verb = "runas";
-
-        if (showlog)
-        {
-            ffp.StartInfo.RedirectStandardError = true;
-            ffp.ErrorDataReceived += new DataReceivedEventHandler(Output);
-        }
-
-        // 開始進程
-        _isRecording = ffp.Start();
-        _pid = ffp.Id;
-
-        if (showlog)
-            ffp.BeginErrorReadLine();
-    }
-
-    private IEnumerator IEExitCmd(Action _OnStopRecording, float stopTime = 3)
-    {
-        // 將當前進程附加到pid進程的控制檯
-        AttachConsole(_pid);
-        // 將控制檯事件的處理句柄設爲Zero，即當前進程不響應控制檯事件
-        // 避免在向控制檯發送【Ctrl C】指令時連帶當前進程一起結束
-        SetConsoleCtrlHandler(IntPtr.Zero, true);
-        // 向控制檯發送 【Ctrl C】結束指令
-        // ffmpeg會收到該指令停止錄製
-        GenerateConsoleCtrlEvent(0, 0);
-
-        // ffmpeg不能立即停止，等待一會，否則視頻無法播放
-        yield return new WaitForSeconds(stopTime);
-
-        // 卸載控制檯事件的處理句柄，不然之後的ffmpeg調用無法正常停止
-        SetConsoleCtrlHandler(IntPtr.Zero, false);
-        // 剝離已附加的控制檯
-        FreeConsole();
-
-        _isRecording = false;
-
-        if (_OnStopRecording != null)
-            _OnStopRecording();
     }
 }
