4db7e4fcfa9710ca263920eaa4f01c186d581a46
john.tc.tsai\DEV <shinn716@livemail.tw>
2022-07-19 16:45:32 +0800

    Put package into project.

---
+++ Assets/Packages/FFScreenAudioRecorder/Runtime/Scripts/FFMergeAV.cs
@@ -0,0 +1,123 @@
+﻿// 
+//  Created by John.Tsai on 2022/2/8
+//  Copyright © 2022 John.Tsai. All rights reserved.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FFMergeAV : MonoBehaviour
+{
+    #region 
+    [DllImport("user32.dll", EntryPoint = "SetWindowText", CharSet = CharSet.Ansi)]
+    public static extern int SetWindowText(int hwnd, string lpString);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+    #endregion
+
+    [Tooltip("StreamingAssetsPath + [FFmpegPath]")]
+    public string ffmpegPath = "/ffmpeg/ffmpeg.exe";
+
+    public string AudioOutput = "X:/test.mp3";
+    public string VideoOutput = "X:/test.mp4";
+
+    public bool showfflog = true;
+
+    public string OutputFolder = "Output";
+
+    [Tooltip("If 'CustomFileName' is empty, it'll export by default name.")]
+    public string CustomFileName = string.Empty;
+
+    public string GetOutputName { get; set; }
+
+    private string ffpath = string.Empty;
+    //private int processId = 0;
+    private Process process = null;
+    private Process process_console = null;
+
+    void Start()
+    {
+        ffpath = @Application.streamingAssetsPath + ffmpegPath;
+
+        if (!Directory.Exists(Path.Combine(Application.streamingAssetsPath, OutputFolder).TrimEnd('/')))
+            Directory.CreateDirectory(Path.Combine(Application.streamingAssetsPath, OutputFolder).TrimEnd('/'));
+
+        IntPtr handle = GetForegroundWindow();
+        SetWindowText(handle.ToInt32(), Application.productName);
+    }
+    private void OnApplicationQuit()
+    {
+        if (process != null)
+        {
+            //process.StandardInput.WriteLine("q");
+            process.WaitForExit();
+            process.Close();
+        }
+
+        if (process_console != null)
+        {
+            //process_console.StandardInput.WriteLine("q");
+            process_console.WaitForExit();
+            process_console.Close();
+        }
+    }
+
+    [ContextMenu("GetDevicesList")]
+    public void GetDevicesList()
+    {
+        var args = "-list_devices true -f dshow -i dummy";
+        Process(ref process_console, args);
+    }
+
+    [ContextMenu("StartMerge")]
+    public void StartMerge()
+    {
+        var path = Path.Combine(Application.streamingAssetsPath, "output");
+        string fileName = CustomFileName.Equals(String.Empty) ?
+                          Path.Combine(path, Application.productName + "_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".mp4") :
+                          Path.Combine(path, CustomFileName + ".mp4");
+        var args = "-i " + VideoOutput + " -i " + AudioOutput + " -c:v copy -c:a aac " + fileName;
+        GetOutputName = fileName;
+
+        var task = Task.Run(() =>
+        {
+            Process(ref process, args);
+        });
+        print("[Log] Output: " + GetOutputName);
+    }
+
+    private void Process(ref Process tprocess, string args)
+    {
+        tprocess = new Process();
+        tprocess.StartInfo.FileName = ffpath;
+
+        // Set args into ffmepg
+        tprocess.StartInfo.Arguments = args;
+        tprocess.StartInfo.UseShellExecute = false;
+        tprocess.StartInfo.RedirectStandardError = true;
+        tprocess.StartInfo.RedirectStandardInput = true;
+        tprocess.StartInfo.RedirectStandardOutput = true;
+        tprocess.StartInfo.CreateNoWindow = true;
+
+        if (showfflog)
+            tprocess.ErrorDataReceived += new DataReceivedEventHandler(Output);
+
+        tprocess.Start();
+        //processId = tprocess.Id;
+        //UnityEngine.Debug.Log("processId:" + processId);
+        tprocess.BeginErrorReadLine();
+    }
+    private void Output(object sendProcess, DataReceivedEventArgs output)
+    {
+        if (!string.IsNullOrEmpty(output.Data))
+            UnityEngine.Debug.Log(output.Data);
+    }
+}
